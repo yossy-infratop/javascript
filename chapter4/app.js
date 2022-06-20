f57429ba3d4768f4e0d7fe82d4d2ebed96b518dc
yossy <yoshikawa.akito.infratop@gmail.com>
2022-06-20 18:25:21 +0900

    chapter4

---
+++ chapter4/app.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {    ＊$(function() {でも可
+//   // jQueryプログラムの内容
+//   $(セレクタ).メソッド(パラメータ);
+// });
+$(document).ready(function () {
+  $('body').html('<h1>Hello jQuery!!</h1>');
+});
